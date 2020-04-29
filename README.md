2a8f5d580d6429159cb8a32ba71ac8f13e2491bf
Superg Uy <Superge Mail>
2020-04-29 08:00:01 +0300

    checkpoint 1

---
+++ README.md
@@ -0,0 +1,4 @@
+ - to do:
+	- read the "city map" from files;
+	- create a real dynamic environment for the agents;
+	- make the agents move around in real time;
---
+++ src/main/scala/agents/UberAgent.scala
@@ -0,0 +1,9 @@
+package agents
+
+class UberAgent(initialNode: Long, cityMap: List[(Long, Long, Double)]) extends IAgent {
+
+  def findTheBestPath(destinationNode: Long): List[Long] = {
+    List(1)
+  }
+
+}
